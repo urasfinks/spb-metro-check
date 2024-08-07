8b3463e99b938abeccb7ec656a2c677488516d35
urasfinks <urasfinks@yandex.ru>
2024-08-07 14:24:02 +0300

    Не должно быть дублей по платежам + добавлен статус процесса

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -9,9 +9,17 @@ CREATE TABLE IF NOT EXISTS "spb-metro-check".tpp
     date_fn timestamp without time zone,
     status character varying(255) COLLATE pg_catalog."default",
     id_transaction character varying(255) COLLATE pg_catalog."default",
+    processed character varying(255),
     data json NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- DROP INDEX IF EXISTS "spb-metro-check".idx_01;
+
+CREATE UNIQUE INDEX IF NOT EXISTS tpp_idx_01
+    ON "spb-metro-check".tpp USING btree
+    (id_transaction COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default;
 
 
 -- DROP TABLE IF EXISTS "spb-metro-check".orange;
@@ -25,3 +33,10 @@ CREATE TABLE IF NOT EXISTS "spb-metro-check".orange
     data json NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- DROP INDEX IF EXISTS "spb-metro-check".orange_idx_01;
+
+CREATE UNIQUE INDEX IF NOT EXISTS orange_idx_01
+    ON "spb-metro-check".orange USING btree
+    (id_transaction COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default;
