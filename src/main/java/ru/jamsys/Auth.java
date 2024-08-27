032b39a0d177b31644571cd809d7cf918ff0ecaa
sfinks <urasfinks@yandex.ru>
2024-08-27 14:19:03 +0300

    Модернизирована Basic авторизация авторизация под ядро

--- src/main/java/ru/jamsys/Auth.java
+++ src/main/java/ru/jamsys/Auth.java
@@ -3,15 +3,12 @@ package ru.jamsys;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import ru.jamsys.core.App;
 import ru.jamsys.core.component.SecurityComponent;
+import ru.jamsys.core.extension.http.HttpAsyncResponse;
+import ru.jamsys.core.extension.http.HttpRequestReader;
 import ru.jamsys.core.web.http.HttpInterceptor;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 
 @Component
 @Lazy
@@ -26,37 +23,18 @@ public class Auth implements HttpInterceptor {
     @Override
     public boolean handle(HttpServletRequest request, HttpServletResponse response) {
         try {
-            String authorization = request.getHeader("Authorization");
-            if (authorization != null && authorization.startsWith("Basic ")) {
-                String[] valueExplode = authorization.split("Basic ");
-                if (valueExplode.length == 2) {
-                    String str = new String(Base64.getDecoder().decode(valueExplode[1]), StandardCharsets.UTF_8);
-                    String user = str.substring(0, str.indexOf(":"));
-                    String password = str.substring(str.indexOf(":") + 1);
-                    String cred = null;
-                    try {
-                        char[] chars = securityComponent.get("web.password." + user);
-                        if (chars != null) {
-                            cred = new String(chars);
-                        }
-                    } catch (Throwable th) {
-                        App.error(th);
-                    }
-                    if (cred != null && cred.equals(password)) {
-                        return true;
-                    }
+            HttpRequestReader.basicAuthHandler(request.getHeader("Authorization"), (user, password) -> {
+                String cred = new String(securityComponent.get("web.password." + user));
+                if (!cred.equals(password)) {
+                    throw new RuntimeException("Incorrect password");
                 }
-            }
-            setUnauthorized(response);
+            });
         } catch (Throwable th) {
             App.error(th);
+            HttpAsyncResponse.setUnauthorized(response);
+            return false;
         }
-        return false;
+        return true;
     }
 
-    public void setUnauthorized(HttpServletResponse response) throws IOException {
-        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        response.setHeader("WWW-Authenticate", "Basic realm=\"JamSys\"");
-        response.getWriter().print("<html><body><h1>401. Unauthorized</h1></body>");
-    }
 }
